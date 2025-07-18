6ce15a72d9a53ed20cbb7618eab9986c7f4a76d8
Wilder Rincon <wildsrincon.developer@gmail.com>
2025-07-18 17:37:47 -0400

    feat: initialize Spring Boot WebFlux application with Maven wrapper and basic configuration

---
+++ src/main/java/org/wildsrincon/webflux/app/ApiSpingbootWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.wildsrincon.webflux.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpingbootWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpingbootWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wildsrincon/webflux/app/ApiSpingbootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wildsrincon.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpingbootWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
